ec03887c45e1cc93d7d45452fed3a9acab5443bc
taso0096 <jin2931121@yahoo.co.jp>
2022-07-17 14:59:30 +0900

    [Update] 複数コネクション対応

--- telnet/server/server.go
+++ telnet/server/server.go
@@ -23,108 +23,102 @@ type Server struct {
 	BufEchoMessage bytes.Buffer
 }
 
-func (s *Server) Listen() error {
-	ln, err := net.Listen("tcp", s.IP+":"+strconv.Itoa(s.Port))
-	if err != nil {
-		return err
-	}
-	defer ln.Close()
-
+func (s *Server) Accept(ln net.Listener) error {
 	conn, err := ln.Accept()
 	if err != nil {
 		return err
 	}
-
 	s.Conn = conn
 	s.Reader = bufio.NewReader(conn)
 	return nil
 }
 
-func (s *Server) ListenAndHandle() error {
-	errChan := make(chan error, 2)
-	s.Reset()
-	err := s.Listen()
+func (s *Server) Handle(ln net.Listener, errChan chan<- error) {
+	err := s.Accept(ln)
 	if err != nil {
-		return err
+		errChan <- err
+		return
 	}
-	defer s.Conn.Close()
-	fmt.Printf("Connected.\n")
+	fmt.Printf("Client Connected.\n")
 
-	// Request TELNET Commands
-	err = s.ReqCmds(s.SupportOptions)
-	if err != nil {
-		return err
-	}
-
-	// Start pty
-	bash := exec.Command("login")
-	s.Ptmx, err = pty.Start(bash)
-	if err != nil {
-		return err
-	}
-	defer s.Ptmx.Close()
-	// Writes pty results to TELNET connection
 	go func() {
-		startIndex := 0
-		byteResult := make([]byte, 4096)
-		for {
-			n, err := s.Ptmx.Read(byteResult)
-			if err != nil {
-				errChan <- err
-				s.Conn.Close()
-				return
-			}
-			if !s.EnableOptions[opt.ECHO] {
-				startIndex = 0
-				for i := 0; i < n; i++ {
-					b, err := s.BufEchoMessage.ReadByte()
-					if b == '\177' {
-						startIndex = n
-						break
-					}
-					if err == io.EOF {
-						break
-					} else if err != nil {
-						errChan <- err
-						s.Conn.Close()
-						return
-					}
-					if strings.Contains("\r\n", string(b)) && strings.Contains("\r\n", string(byteResult[i])) {
-						startIndex++
-						i++
-					} else if b != byteResult[i] {
-						break
-					}
-					startIndex++
-				}
-				s.BufEchoMessage.Reset()
-			}
-			if startIndex < n {
-				s.WriteBytes(byteResult[startIndex:n])
-			}
-		}
-	}()
+		defer s.Conn.Close()
 
-	for {
-		byteMessage, err := s.ReadMessage()
+		// Request TELNET Commands
+		err = s.ReqCmds(s.SupportOptions)
 		if err != nil {
 			errChan <- err
-			break
+			return
 		}
-		if byteMessage == nil {
-			continue
+
+		// Start pty
+		bash := exec.Command("login")
+		s.Ptmx, err = pty.Start(bash)
+		if err != nil {
+			errChan <- err
+			return
 		}
-		if !s.EnableOptions[opt.ECHO] {
-			s.BufEchoMessage.Write(byteMessage)
+		defer s.Ptmx.Close()
+		// Relay output from pty to client
+		go s.ReadPty(errChan)
+
+		// Relay input from client to pty
+		for {
+			byteMessage, err := s.ReadMessage()
+			if err != nil {
+				errChan <- err
+				break
+			}
+			if byteMessage == nil {
+				continue
+			}
+			if !s.EnableOptions[opt.ECHO] {
+				s.BufEchoMessage.Write(byteMessage)
+			}
+			s.Ptmx.Write(byteMessage)
 		}
-		s.Ptmx.Write(byteMessage)
-	}
-	return <-errChan
+	}()
 }
 
-func (s *Server) Reset() {
-	s.EnableOptions = map[byte]bool{}
-	s.BufEchoMessage = *new(bytes.Buffer)
+func (s *Server) ReadPty(errChan chan<- error) {
+	startIndex := 0
+	byteResult := make([]byte, 4096)
+	for {
+		n, err := s.Ptmx.Read(byteResult)
+		if err != nil {
+			errChan <- err
+			s.Conn.Close()
+			return
+		}
+		// Exclude client input if echo option is not enabled
+		if !s.EnableOptions[opt.ECHO] {
+			startIndex = 0
+			for i := 0; i < n; i++ {
+				b, err := s.BufEchoMessage.ReadByte()
+				if b == '\177' {
+					startIndex = n
+					break
+				}
+				if err == io.EOF {
+					break
+				} else if err != nil {
+					errChan <- err
+					s.Conn.Close()
+					return
+				}
+				if strings.Contains("\r\n", string(b)) && strings.Contains("\r\n", string(byteResult[i])) {
+					i++
+				} else if b != byteResult[i] {
+					break
+				}
+				startIndex = i + 1
+			}
+			s.BufEchoMessage.Reset()
+		}
+		if startIndex < n {
+			s.WriteBytes(byteResult[startIndex:n])
+		}
+	}
 }
 
 func Init(ip string, port int, supportOptions []byte) Server {
@@ -133,19 +127,34 @@ func Init(ip string, port int, supportOptions []byte) Server {
 	s.Port = port
 	s.SupportOptions = supportOptions
 	s.BuildCmdRes = BuildCmdRes
-	s.Reset()
+	s.EnableOptions = map[byte]bool{}
+	s.BufEchoMessage = *new(bytes.Buffer)
 	return s
 }
 
 func Run(ip string, port int) {
-	// Init TELNET Server
-	supportOptions := []byte{opt.ECHO, opt.SUPPRESS_GO_AHEAD, opt.NEGOTIATE_ABOUT_WINDOW_SIZE}
-	s := Init(ip, port, supportOptions)
-
+	// Listen TCP
+	ln, err := net.Listen("tcp", ip+":"+strconv.Itoa(port))
+	if err != nil {
+		log.Fatal("Listen Error:", err)
+	}
+	defer ln.Close()
 	fmt.Printf("Listen on %s:%d...\n", ip, port)
+
+	// Logging errors
+	errChan := make(chan error, 2)
+	defer close(errChan)
+	go func() {
+		for err := range errChan {
+			log.Println("Handle Error:", err)
+		}
+	}()
+
+	// Handle connections
+	supportOptions := []byte{opt.ECHO, opt.SUPPRESS_GO_AHEAD, opt.NEGOTIATE_ABOUT_WINDOW_SIZE}
 	for {
-		err := s.ListenAndHandle()
-		log.Println("ListenAndHandle Error:", err)
+		s := Init(ip, port, supportOptions)
+		s.Handle(ln, errChan)
 	}
 }
 
